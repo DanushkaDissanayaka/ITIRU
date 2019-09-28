79ffa7f6b18fba0e6ae2d315efcdc19eee3c295f
donshack <sandaruwan.15@itfac.mrt.ac.lk>
2019-09-28 14:30:25 +0530

    Create the keynote speakers component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { KeynoteSpeakersComponent } from './keynote-speakers/keynote-speakers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    KeynoteSpeakersComponent
   ],
   imports: [
     BrowserModule,
