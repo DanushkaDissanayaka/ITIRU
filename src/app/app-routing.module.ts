36162c197db7d4c0fa7a7912ab9fd5543219aa07
donshack <sandaruwan.15@itfac.mrt.ac.lk>
2019-09-29 10:06:18 +0530

    Add kenote to the route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { KeynoteSpeakersComponent } from './keynote-speakers/keynote-speakers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"keynote-speakers", component: KeynoteSpeakersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { KeynoteSpeakersComponent } from './keynote-speakers/keynote-speakers.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    KeynoteSpeakersComponent,
     FooterComponent
   ],
   imports: [
