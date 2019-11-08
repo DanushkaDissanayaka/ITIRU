37a6eee068caa191fe2ba911fbec56ffec5512ee
Danushka <danushka@bourketechnologies.com>
2019-11-08 21:16:09 +0530

    Importrent dates change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -31,11 +31,11 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.news1 = new NewsCardModel
-    this.news1.Date = "28 SEP 2019"
+    this.news1.Date = "08 Nov 2019"
     this.news1.Title = "Submission is now open"
     this.news1.Message = `<p>Submit your paper <a href = 'https://easychair.org/conferences/?conf=icitr2019'>here</a>,
                          before you submit please read the Author Instructions. <br>
-                         Submission is due on 31<sup>th</sup> October 2019.
+                         Submission is due on 15<sup>th</sup>November 2019.
                          </p>`
   }
 
